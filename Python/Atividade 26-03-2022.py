99881a93afcbc19f14a5e577466d3c4abdfc47e8
Jefferson Teles <58746739+JeffersonTeles314@users.noreply.github.com>
2022-06-12 14:01:37 -0500

    Update and rename Atividade 26-03-2022.txt to Atividade 26-03-2022.py

--- Python/Atividade 26-03-2022.py
+++ Python/Atividade 26-03-2022.py
@@ -1,30 +1,22 @@
 
-Questão 1
+#Questão 1
 var1 = int(input("Por Favor Insira um Número Inteiro:"))
 print("O Número Inteiro Inserido foi: %i" %var1)
 
----
 
-Questão 2
+#Questão 2
 var1 = int(input("Por Favor Insira um Número Inteiro:"))
 var2 = int(input("Por Favor Insira Outro Número Inteiro:"))
 soma = var1 + var2
 print("A Soma dos Números Inteiros Inseridos foi: %i" %soma)
 
----
 
-Questão 3
+#Questão 3
 var1 = int(input("Por Favor Insira o Lavor do Lado do Quadrado:"))
 print("O Valor da Área do Quadrdo foi: %i. Equanto o Valor do Dobro da Área é Igual há: %i" %(var1,var1*2))
 
----
 
-Questão 4
+#Questão 4
 var1 = int(input("Por Favor Insira o Valor da Temperatura em Fahrenheit:"))
 var2 = 5*((Var1-32)/9)
 print("%i Graus Fahrenheit, é igaul há %i Graus Celsius" %(var1,var2))
-
-
-
-
-
