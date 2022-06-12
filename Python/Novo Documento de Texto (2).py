59277bfa2d2b3a4b6a1b01d62c46f9fdadae6f01
Jefferson Teles <58746739+JeffersonTeles314@users.noreply.github.com>
2022-06-12 14:03:13 -0500

    Update and rename Novo Documento de Texto (2).txt to Novo Documento de Texto (2).py

--- Python/Novo Documento de Texto (2).py
+++ Python/Novo Documento de Texto (2).py
@@ -1,9 +1,9 @@
-----1----
+#----1----
 var1 = float(input("Quanto Vale a Hora de Seu Trabalho:"))
 var2 = float(input("Quantas Horas Você trabalha Por Mês:"))
 print(f"Você Ganha {var1*var2} reais por mês.")
 
-----2----
+#----2----
 var1 = int(input("Insira um Número Inteiro:"))
 var2 = int(input("Insira Outro Número Inteiro:"))
 var3 = float(input("Insira Um Número Real:"))
@@ -11,7 +11,7 @@ print(f"O produto do dobro do primeiro com metade do segundo: {var1*var2}")
 print(f"A soma do triplo do primeiro com o terceiro: {3*var1+var3}")
 print(f"O terceiro elevado ao cubo: {var3**3}")
 
-----3----
+#----3----
 psototl= float(input("Insira o Peso da Pesca:"))
 if psototl > 50:
     excesso = psototl - 50
@@ -20,7 +20,7 @@ if psototl > 50:
 else:
     print("O Peso Tá Tranquilo")
 
-----4----
+#----4----
 var1 = float(input("Quanto Vale a Hora de Seu Trabalho:"))
 var2 = float(input("Quantas Horas Você trabalha Por Mês:"))
 print(f"+ Salário Bruto : {var1*var2} R$")
@@ -33,20 +33,20 @@ print(f"- Sindicato ( 5%) : {Sindicato} R$")
 SALL = (var1*var2) * 0.76
 print(f"= Salário Liquido : {SALL} R$")
 
-----5----
+#----5----
 from math import ceil
 metrosE2 = float(input("Quantos Metros Quadrados Devem ser Pintados:"))
 litros = metrosE2/3
 latas= ceil(litros/18)
 print(f"Serão Necessárias {latas} Latas Custando {latas*80} R$")
 
-----6----
+#----6----
 megas = float(input("Quantidade de Megas do Arquivo:"))
 velocidade = float(input("Velocidade Média do Dowload em Mbps: "))
 tempo = megas/(velocidade*60)
 print(f"Serão Necessários {tempo} Minutos de Dowload")
 
-----7----
+#----7----
 Valor1 = float(input("Valor1: "))
 Valor2 = float(input("Valor2: "))
 if Valor1 > Valor2:
@@ -56,7 +56,7 @@ elif Valor1 < Valor2:
 else:
     print(f"Os Valores {Valor1} e {Valor2} são Iguais.")
 
-----8----
+#----8----
 var = float(input("Valor: "))
 if var > 0:
     print(f"O Valor {var} é Positivo")
@@ -65,7 +65,7 @@ elif var < 0:
 else:
     print(f"O Valor {var} e Igual a Zero")
 
-----9----
+#----9----
 var = input("Valor: ")
 if var == "F" or var == "f":
     print(f"F - Feminino")
@@ -74,7 +74,7 @@ elif var == "M" or var == "m":
 else:
     print(f"Sexo Inválido.")
 
-----10----
+#----10----
 var = input("Insira uma Letra: ")
 var = var.upper()
 vogl = {"A", "E", "I", "O", "U"}
