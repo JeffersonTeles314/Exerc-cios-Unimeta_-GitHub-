84a02f47140d3ebbb7e3c320a62d9adc39de434b
Jefferson Teles <58746739+JeffersonTeles314@users.noreply.github.com>
2022-06-12 14:02:21 -0500

    Update and rename Atividade Python 06-06-2022.txt to Atividade 06-06-2022.py

--- Python/Atividade 06-06-2022.py
+++ Python/Atividade 06-06-2022.py
@@ -1,10 +1,10 @@
-Questão 1
+#Questão 1
 def npress1(varn):
     for n in range(1,varn+1):
         var1 = str(n)
         print((var1+" ")*n)
 
-Questão 2
+#Questão 2
 def npress2(varn):
     for n in range(1,varn+1):
         for m in range(1,n):
@@ -12,13 +12,13 @@ def npress2(varn):
             print(var1+" ", end='')
         print(n)
 
-Questão 3
+#Questão 3
 def triplensum(var1,var2,var3):
     return var1+var2+var3
 
 print(triplensum(1,2,3))
 
-Questão 4
+#Questão 4
 def signalcheck(var1):
     if not (type(var1) == int or type(var1) == float):
         return "Valor Inválido"
@@ -29,12 +29,12 @@ def signalcheck(var1):
     else:
         return "Valor Inválido"
 
-Questão 5
+#Questão 5
 def somaImposto(valor,taxaImposto):
     taxaImposto /= 100
     return valor + (valor*taxaImposto)
 
-Questão 6
+#Questão 6
 def valorPagamento(valorPrest,diasAtraso):
     return (1.03 + diasAtraso*0.001)*valorPrest
 
